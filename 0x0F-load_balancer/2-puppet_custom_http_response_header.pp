fed1a5e9bfe50e163daa5a68a94a58cf2a3c5532
mouezelaifa <mouezelaifa@gmail.com>
2023-10-02 11:25:17 -0700

    0x0F. Load balancer

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,35 +1,25 @@
-# instal and configure nginx
-exec { 'apt-update':
-  command     => 'apt-get -y update',
-  path        => '/usr/bin:/bin',
-  refreshonly => true,
-}
-exec { 'apt-get':
-  command => 'apt-get -y install nginx',
-  path    => '/usr/bin:/bin',
-  require => Exec['apt-update'],
+# Installs a Nginx server with custome HTTP header
+
+exec {'update':
+  provider => shell,
+  command  => 'sudo apt-get -y update',
+  before   => Exec['install Nginx'],
 }
 
--> file { '/var/www/html/index.nginx-debian.html':
-  content => 'Hello World!',
+exec {'install Nginx':
+  provider => shell,
+  command  => 'sudo apt-get -y install nginx',
+  before   => Exec['add_header'],
 }
 
-file_line { 'add_redirect':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => 'server_name _;',
-  line   => "\tlocation /redirect_me {return 301 https://www.youtube.com/watch?v=QH2-TGUlwu4;}",
-  notify => Exec['restart_nginx'],
+exec { 'add_header':
+  provider    => shell,
+  environment => ["HOST=${hostname}"],
+  command     => 'sudo sed -i "s/include \/etc\/nginx\/sites-enabled\/\*;/include \/etc\/nginx\/sites-enabled\/\*;\n\tadd_header X-Served-By \"$HOST\";/" /etc/nginx/nginx.conf',
+  before      => Exec['restart Nginx'],
 }
 
-file_line { 'add_header':
-  path   => '/etc/nginx/sites-enabled/default',
-  after  => "^\tlocation / {",
-  line   => "\t\tadd_header X-Served-By \"${::hostname}\";",
-  notify => Exec['restart_nginx'],
-}
-
-exec { 'restart_nginx':
-  command     => '/usr/sbin/service nginx restart',
-  refreshonly => true,
-  subscribe   => File_line['add_redirect'],
+exec { 'restart Nginx':
+  provider => shell,
+  command  => 'sudo service nginx restart',
 }
