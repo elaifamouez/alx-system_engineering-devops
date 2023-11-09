23eb1287b3d114df8d4851d23074c9d6d805e17f
mouezelaifa <mouezelaifa@gmail.com>
2023-11-09 15:03:53 -0800

    0x16. API advanced

--- 0x16-api_advanced/100-count.py
+++ 0x16-api_advanced/100-count.py
@@ -1,59 +1,57 @@
 #!/usr/bin/python3
-"""
-This module is for task 3, which involves querying the Reddit API and
-returning the count of words in word_list in the titles of all the hot posts
-of the subreddit.
-"""
+"""Recursive function that queries the Reddit API"""
+import requests
+import sys
+import re
 
 
-def count_words(subreddit, word_list, word_count={}, after=None):
-    """
-    Queries the Reddit API and returns the count of words in word_list in
-    the titles of all the hot posts of the subreddit.
+def count_words(subreddit, word_list, after=None, word_counts=None):
+    """ Defines words count"""
+    if word_counts is None:
+        word_counts = {}
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    headers = {
+        'User-Agent': '100-count'
+    }
+    params = {'after': after} if after else {}
+    response = requests.get(url, headers=headers, params=params)
 
-    :param subreddit: The name of the subreddit to query.
-    :param word_list: A list of words to count in post titles.
-    :param word_count: A dictionary to store the word counts.
-    :param after: The "after" parameter for pagination.
-    :return: None if an error occurs,
-    or it prints the word counts in descending order.
-    """
-    import requests
+    if response.status_code == 200:
+        try:
+            post_data = response.json()['data']['children']
+            for post in post_data:
+                title = post['data']['title']
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/hot.json"
-                            .format(subreddit),
-                            params={"after": after},
-                            headers={"User-Agent": "My-User-Agent"},
-                            allow_redirects=False)
-    if sub_info.status_code != 200:
+                normalized_title = re.sub(r'[^a-zA-Z0-9 ]', '',
+                                          title.lower())
+                words = normalized_title.split()
+
+                for word in words:
+                    if word in word_list:
+                        if word in word_counts:
+                            word_counts[word] += 1
+                        else:
+                            word_counts[word] = 1
+
+            after = response.json()['data']['after']
+            if after is not None:
+                return count_words(subreddit, word_list, word_counts, after)
+            else:
+                sorted_word_counts = sorted(word_counts.items(),
+                                            key=lambda item:
+                                            (-item[1], item[0]))
+                for keyword, count in sorted_word_counts:
+                    print(f"{keyword}: {count}")
+        except (KeyError, ValueError):
+            return None
+    elif response.status_code == 302:
         return None
-
-    info = sub_info.json()
-
-    hot_l = [child.get("data").get("title")
-             for child in info
-             .get("data")
-             .get("children")]
-    if not hot_l:
-        return None
-
-    word_list = list(dict.fromkeys(word_list))
-
-    if word_count == {}:
-        word_count = {word: 0 for word in word_list}
-
-    for title in hot_l:
-        split_words = title.split(' ')
-        for word in word_list:
-            for s_word in split_words:
-                if s_word.lower() == word.lower():
-                    word_count[word] += 1
-
-    if not info.get("data").get("after"):
-        sorted_counts = sorted(word_count.items(), key=lambda kv: kv[0])
-        sorted_counts = sorted(word_count.items(),
-                               key=lambda kv: kv[1], reverse=True)
-        [print('{}: {}'.format(k, v)) for k, v in sorted_counts if v != 0]
     else:
-        return count_words(subreddit, word_list, word_count,
-                           info.get("data").get("after"))
+        return None
+
+
+if __name__ == "__main__":
+    if len(sys.argv) > 2:
+        subreddit = sys.argv[2]
+        word_list = sys.argv[3]
+        count_words(subreddit, word_list)
