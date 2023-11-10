7f582b61b099022a31f880d2c239db680a9ee9ee
mouezelaifa <mouezelaifa@gmail.com>
2023-11-09 21:54:04 -0800

    0x16. API advanced

--- 0x16-api_advanced/2-recurse.py
+++ 0x16-api_advanced/2-recurse.py
@@ -1,38 +1,26 @@
 #!/usr/bin/python3
-""" module of the function recurse """
-
+"""
+this doc for module
+"""
 import requests
 
 
-def recurse(subreddit, hot_list=[], after=""):
-    """
-    Recursively retrieve and append the titles of the top hot posts for
-    a given subreddit using the Reddit API.
-
-    Args:
-        subreddit (str): The name of the subreddit for which to retrieve
-                        the top hot posts.
-        hot_list (list): A list to which the post titles are appended.
-                        Used for recursive calls.
-        after (str): The Reddit API 'after' parameter, which is used to
-                    navigate through pagination.
-
-    Returns:
-        list: A list containing the titles of the top hot posts from the
-            specified subreddit.
-            Returns an empty list if the subreddit is not found.
-    """
-    if subreddit is None:
-        return None
-    url = "http://www.reddit.com/r/{}/hot.json".format(subreddit)
-    user_agent = {"User-Agent": "ALX project about advanced api"}
-
-    response = requests.get(url, params={"after": after}, headers=user_agent)
-
+def recurse(subreddit, after=None):
+    """method doc"""
+    headers = {"User-Agent": "MyCustomUserAgent/1.0"}
+    params = {"after": after}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    response = requests.get(url, allow_redirects=False,
+                            headers=headers, params=params)
+    all_posts = []
     if response.status_code == 200:
-        after = response.json().get("data").get("after")
-        if not after:
-            return hot_list
-        for post in response.json().get("data").get("children"):
-            hot_list.append(post.get("data").get("title"))
-        return recurse(subreddit, hot_list, after)
+        data = response.json()
+        after = data["data"]["after"]
+        if after is None:
+            return all_posts
+        for post in data["data"]["children"]:
+            all_posts.append(post["data"]["title"])
+        next = recurse(subreddit, after)
+        all_posts.extend(next)
+        return all_posts
+    return None
